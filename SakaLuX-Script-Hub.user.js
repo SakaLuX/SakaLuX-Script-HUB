d7683e70e30a322d3afd20c6abeb6a64416159df
SakaLuX <sakalutzu92@yahoo.com>
2026-08-23 23:25:49 +0300

    Create SakaLuX-Script-Hub.user.js

---
+++ SakaLuX-Script-Hub.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         SakaLuX Script Hub
+// @namespace    sakalux.script.hub
+// @version      1.6.0
+// @description  Central manager, launcher and health monitor for SakaLuX Torn scripts, with quick actions, favorites, search, backup and mobile/PDA support.
+// @author       SakaLuX
+// @match        https://www.torn.com/*
+// @grant        GM_xmlhttpRequest
+// @connect      update.greasyfork.org
+// @license      MIT
+// @downloadURL  https://update.greasyfork.org/scripts/592699/SakaLuX%20Script%20Hub.user.js
+// @updateURL    https://update.greasyfork.org/scripts/592699/SakaLuX%20Script%20Hub.meta.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const VERSION = '1.6.0';
+})();
